2ece3e177f30c14dd5b75bb8cce9cdb3f6a39ea3
Kostyantyn <kostp62020@gmail.com>
2021-05-20 17:55:10 +0300

    pull code with origin master and refactoring code

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,24 +1,23 @@
 Capybara.javascript_driver = :webkit # Selenium works, but not as well
- 
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
-   Debug.rb
   # Use transactions by default
-  config.before :each do
+  config.before do
     DatabaseCleaner.strategy = :transaction
   end
 
   # For the javascript-enabled tests, switch to truncation, but *only on tables that were used*
-  config.before :each, :js => true do
-    DatabaseCleaner.strategy = :truncation, {:pre_count => true}
+  config.before :each, js: true do
+    DatabaseCleaner.strategy = :truncation, { pre_count: true }
   end
 
-  config.before :each do
+  config.before do
     DatabaseCleaner.start
   end
 
-  config.after :each do
+  config.after do
     DatabaseCleaner.clean
   end
 end
