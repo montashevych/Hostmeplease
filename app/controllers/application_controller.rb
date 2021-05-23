af27858b888c90e6d0430822a6396279448d83ee
Kostyantyn <kostp62020@gmail.com>
2021-05-23 12:16:11 +0300

    moved access restrictions to an unauthorized user to certain sections of the site in Users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
@@ -10,4 +8,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
+
 end
