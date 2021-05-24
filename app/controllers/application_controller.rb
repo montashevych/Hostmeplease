d1b4f3696bdd5ea9675e085371e447b7f9326893
Kostyantyn <kostp62020@gmail.com>
2021-05-24 18:45:27 +0300

    Bug fixes rubocop code analyzer fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected
 
   def configure_permitted_parameters
-    attributes = [:name, :surname,:username, :email, :avatar]
+    attributes = [:name, :surname,  :email, :avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
-
 end
