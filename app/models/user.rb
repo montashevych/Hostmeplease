a698fd22e2cbf116ba386758457ec75962d62468
Kostyantyn <kostp62020@gmail.com>
2021-05-25 09:08:46 +0300

    add enum role for registration user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: { consumer: 'consumer', owner: 'owner' }         
 end
