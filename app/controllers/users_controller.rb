59126a6e6c5eb9850355eef2253f368d13aeb8b8
Kostyantyn <kostp62020@gmail.com>
2021-05-25 11:13:58 +0300

    Removed currently unnecessary actions and refactoring code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    @users = current_user
   end
 
   def show
+  end
+
+  def destroy
+    user_find
+    @user.destroy!
+    redirect_to users_path
+  end
+
+  private
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:FirstName, :LastName, :role, :email)
   end
 end
