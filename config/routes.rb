e78a5f086ff7e6ba14e467697ec233f25be6c892
Ivan <vanuha266@gmail.com>
2021-05-25 10:37:27 +0300

    was converted to slim html & added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'places/index'
+  resources :places
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
