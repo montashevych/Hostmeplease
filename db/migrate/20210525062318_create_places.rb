68211c73febbc9c7c9293b9a5970dd9b796754bd
Ivan <vananovak@ukr.net>
2021-05-25 09:40:09 +0300

    Updated schema

--- db/migrate/20210525062318_create_places.rb
+++ db/migrate/20210525062318_create_places.rb
@@ -1,6 +1,11 @@
 class CreatePlaces < ActiveRecord::Migration[6.1]
   def change
     create_table :places do |t|
+      t.string :title
+      t.string :place_type
+      t.text :description
+      t.float :price
+      t.boolean :active
 
       t.timestamps
     end
