f2b18152f3c5f3a4eb9678e98aea47a5aaa5d21a
Kostyantyn <kostp62020@gmail.com>
2021-05-27 12:12:09 +0300

    add action new create with functional and change functional at looked user for views and add success strong params for database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
   def index
-    @users = current_user
+    @users = User.all
   end
 
   def show
-    @user = current_user
+    user_find
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      UserMailer.with(user: @user).new_user_email.deliver_later
+
+      flash[:success] = t('flash.user.success')
+      redirect_to @user
+    else
+      flash.now[:error] = t ('flash.user.error_html')
+      render :new
+  end
+end
 
   private
 
@@ -16,6 +32,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:FirstName, :LastName, :role, :email)
+    params.require(:user).permit( :FirstName, :LastName, :role, :email, :message, :password, :password_confirmation  )
   end
 end
