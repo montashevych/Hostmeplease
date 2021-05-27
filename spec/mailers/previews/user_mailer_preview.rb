507ac064e499a265e8279975f49afef43a54c22f
Kostyantyn <kostp62020@gmail.com>
2021-05-27 11:56:49 +0300

    Configure the local UserMailer to send an email to admin about creating a new user

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  def new_user_email
+    # Set up a temporary user for the preview
+    user = User.new(FirstName: "Ivan", LastName: "Bogun", email: "ivan@gmail.com", message: "I want to log in!")
+
+    UserMailer.with(user: user).new_user_email
+  end
+end
