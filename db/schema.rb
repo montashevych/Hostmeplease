1de02d7cee568e9d3dc858185bf3dac045907528
Kostyantyn <kostp62020@gmail.com>
2021-05-27 11:42:24 +0300

    add migration for users and add index for users_email and add password_digest for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_081549) do
+ActiveRecord::Schema.define(version: 2021_05_26_134932) do
 
   create_table "users", force: :cascade do |t|
     t.string "FirstName", default: "", null: false
     t.string "LastName", default: "", null: false
     t.string "role", default: "consumer", null: false
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
+    t.text "message", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
