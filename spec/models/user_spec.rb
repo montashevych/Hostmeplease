985bf3ef64cf8e263bf99b390f7edfaa3547eb33
Kostyantyn <kostp62020@gmail.com>
2021-05-30 15:04:54 +0300

    add four rspec tests for model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
-Rspec.describe User do
+RSpec.describe User, type: :model do
+  it 'is valid with valid attributes' do
+    @user1 = described_class.find_by(user: @user.email)
+    expect(@user1).to be_valid?
+  end
+
+  it 'has a unique email' do
+    user2 = build(:user, email: 'bob@gmail.com')
+    expect(user2).not_to be_valid
+  end
+
+  it 'is not valid without a password' do
+    user2 = build(:user, password: nil)
+    expect(user2).not_to be_valid
+  end
+
+  it 'is not valid without an email' do
+    user2 = build(:user, email: nil)
+    expect(user2).not_to be_valid
+  end
 end
