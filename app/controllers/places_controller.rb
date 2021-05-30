9390f7c8c3f3f138d47d242c901a0bc520eb0ab0
Ivan <vanuha266@gmail.com>
2021-05-30 21:15:41 +0300

    was protected sql require

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.where(active: true).paginate(page: params[:page], per_page: 9)
+    @places = Place.where("active = ?", true).paginate(page: params[:page], per_page: 9)
   end
 end
