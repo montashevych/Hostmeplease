65ef7b2da19603c90c9ecf388e16548349f945b7
Ivan <vanuha266@gmail.com>
2021-05-30 12:03:08 +0300

    fix channals error

--- app/frontend/packs/site.js
+++ app/frontend/packs/site.js
@@ -6,10 +6,9 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import 'bootstrap' 
-import "channels"
+import 'bootstrap'
+import "javascript/channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
