996e01d0e43867d235e8d7a4cfa05f4ad5c7d6dd
RMuskovets <rmuskovets@gmail.com>
2021-05-31 18:12:20 +0300

    Use `bootstrap_js_files.js` for Bootstrap JS

--- app/frontend/packs/site.js
+++ app/frontend/packs/site.js
@@ -6,7 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import 'bootstrap' 
+import '../js/bootstrap_js_files.js'
 import "channels"
 
 Rails.start()
