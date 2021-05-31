a04ee3246d8090eef25e287289f4df479f4df325
Ivan <vanuha266@gmail.com>
2021-05-31 09:35:53 +0300

    was added some tests for controller

---
+++ spec/controllers/places_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PlacesController do
+  describe "GET index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "return HTML" do
+      get :index
+      expect(response.media_type).to eq "text/html"
+    end
+  end
+end
