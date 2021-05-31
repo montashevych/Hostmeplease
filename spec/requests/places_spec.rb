64b75518be54eb0a27d1173d953923785d7b4471
Ivan <vanuha266@gmail.com>
2021-05-31 14:22:12 +0300

    was removed dublicate test

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -8,12 +8,5 @@ RSpec.describe 'Places', type: :request do
         expect(response).to have_http_status(:success)
       end
     end
-
-    context 'when template was created and does not removed' do
-      it 'template was found' do
-        get '/places'
-        expect(response).not_to have_http_status(:not_found)
-      end
-    end
   end
 end
