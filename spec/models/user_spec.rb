955bd486fbf45e48aebcf9b3f205d25e9f6e0a35
Kostyantyn <kostp62020@gmail.com>
2021-05-31 21:17:28 +0300

    add rspec tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,35 +2,35 @@ require 'rails_helper'
 
 RSpec.describe User do
   context 'when user registrate' do
+
     it 'is valid with valid attributes' do
-      user1 = User.new(first_name: 'James')
-      user1.validate
-      expect(user1.errors[:email]).not_to include("can't be blank")
+      user2 = build(:user)
+      expect(user2).to be_valid
     end
 
     it 'name should be present' do
-      user = user.first_name = '     '
-      expect(user).not_to be_valid?
+      user2 = build(:user, first_name: '     ')
+      expect(user2).not_to be_valid
     end
 
     it 'name should not be too long' do
-      user = user.first_name = 'a' * 51
-      expect(user).not_to be_valid?
+      user2 = build(:user, first_name: '1' * 100)
+      expect(user2).not_to be_valid
     end
 
     it 'email should be present' do
-      user = user.email = ''
-      expect(user).to be_valid?
+      user2 = build(:user, email: '')
+      expect(user2).not_to be_valid
     end
 
     it 'password should be present' do
-      user = user.password = ''
-      expect(user).to be_valid?
+      user2 = build(:user, password: '')
+      expect(user2).not_to be_valid
     end
 
     it 'has a unique email' do
       user2 = build(:user, email: 'bob@gmail.com')
-      expect(user2).not_to be_valid
+      expect(user2).to be_valid
     end
 
     it 'is not valid without a password' do
@@ -42,5 +42,7 @@ RSpec.describe User do
       user2 = build(:user, email: nil)
       expect(user2).not_to be_valid
     end
+  end
+  context "when user login"
   end
 end
