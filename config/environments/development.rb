1af5897f03293e30496ee1026e90951ed9a1044e
Serhii_Montashevych <serhiimontashevych@gmail.com>
2021-05-31 17:44:44 +0300

    change env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.hosts.clear
-  
+  config.hosts << "pacific-mountain-49875.herokuapp.com"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
