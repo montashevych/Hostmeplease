9e51e9249d5590e19e58f72439f379b9823b6bd6
Kostyantyn <kostp62020@gmail.com>
2021-05-31 20:05:49 +0300

    add rspec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :user do
     id        { 1 }
-    FirstName { 'James' }
-    LastName  { 'Bond' }
-    password  { '123456' }
-    email     { 'ivan@gmail.com' }
-    role      { 'consumer' }
+    first_name { "James" }
+    password  { "123456" }
+    email     { "ivan@gmail.com" }
+    role      { "consumer" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it 'is valid with valid attributes' do
-    @user1 = described_class.find_by(user: @user.email)
-    expect(@user1).to be_valid?
-  end
+RSpec.describe User do
+  context "When User registrate" do
 
-  it 'has a unique email' do
-    user2 = build(:user, email: 'bob@gmail.com')
-    expect(user2).not_to be_valid
-  end
+    it "is valid with valid attributes" do
+      user1 = User.new(first_name: "James")
+      user1.validate
+      expect(user1.errors[:email]).not_to include("can't be blank") 
+    end
 
-  it 'is not valid without a password' do
-    user2 = build(:user, password: nil)
-    expect(user2).not_to be_valid
-  end
+    it "name should be present" do
+      user = @user.first_name = "     "
+      expect(user).not_to be_valid?
+    end
 
-  it 'is not valid without an email' do
-    user2 = build(:user, email: nil)
-    expect(user2).not_to be_valid
+    it "name should not be too long" do
+      user = @user.first_name = "a" * 51
+      expect(user).not_to be_valid?
+    end
+
+    it "email should be present" do
+      user = @user.email = ""
+      expect(user).to be_valid?
+    end
+
+     it "password should be present" do
+      user = @user.password = ""
+      expect(user).to be_valid?
+    end
+
+    it "has a unique email" do
+      user2 = build(:user, email: "bob@gmail.com")
+      expect(user2).not_to be_valid
+    end
+
+    it "is not valid without a password" do
+      user2 = build(:user, password: nil)
+      expect(user2).not_to be_valid
+    end
+
+    it "is not valid without an email" do
+      user2 = build(:user, email: nil)
+      expect(user2).not_to be_valid
+    end
+
+    
   end
 end
