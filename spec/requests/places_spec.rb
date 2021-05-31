a0606484fe31c61aa64e2593cf49dec6a87d2d54
Ivan <vanuha266@gmail.com>
2021-05-31 09:34:59 +0300

    was added tests for requests

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Places", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/places/"
-      expect(response).to have_http_status(:success)
-    end
+  before(:all) do
+    get "/places"
   end
 
+  describe "GET /index" do
+    context "with successfull status" do
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'template was created and does not removed' do
+      it "template was found" do
+        expect(response).not_to have_http_status(:not_found)
+      end
+    end
+  end
 end
