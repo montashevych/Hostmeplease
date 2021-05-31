af3c0efc3af397681378fd1ddea7f27e5fca4b04
Kostyantyn <kostp62020@gmail.com>
2021-05-31 20:09:39 +0300

    fixed rubocop errors in user spec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,49 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  context "When User registrate" do
-
-    it "is valid with valid attributes" do
-      user1 = User.new(first_name: "James")
+  context 'when user registrate' do
+    it 'is valid with valid attributes' do
+      user1 = User.new(first_name: 'James')
       user1.validate
-      expect(user1.errors[:email]).not_to include("can't be blank") 
+      expect(user1.errors[:email]).not_to include("can't be blank")
     end
 
-    it "name should be present" do
-      user = @user.first_name = "     "
+    it 'name should be present' do
+      user = user.first_name = '     '
       expect(user).not_to be_valid?
     end
 
-    it "name should not be too long" do
-      user = @user.first_name = "a" * 51
+    it 'name should not be too long' do
+      user = user.first_name = 'a' * 51
       expect(user).not_to be_valid?
     end
 
-    it "email should be present" do
-      user = @user.email = ""
+    it 'email should be present' do
+      user = user.email = ''
       expect(user).to be_valid?
     end
 
-     it "password should be present" do
-      user = @user.password = ""
+    it 'password should be present' do
+      user = user.password = ''
       expect(user).to be_valid?
     end
 
-    it "has a unique email" do
-      user2 = build(:user, email: "bob@gmail.com")
+    it 'has a unique email' do
+      user2 = build(:user, email: 'bob@gmail.com')
       expect(user2).not_to be_valid
     end
 
-    it "is not valid without a password" do
+    it 'is not valid without a password' do
       user2 = build(:user, password: nil)
       expect(user2).not_to be_valid
     end
 
-    it "is not valid without an email" do
+    it 'is not valid without an email' do
       user2 = build(:user, email: nil)
       expect(user2).not_to be_valid
     end
-
-    
   end
 end
