5700f15ef5953c8af76783e8fefc14a3ff4e5b6d
Serhii_Montashevych <serhiimontashevych@gmail.com>
2021-06-01 10:00:04 +0300

    add tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-  factory :valid_user do
-    id { 1 }
-    first_name { 'James' }
-    password  { '123456' }
-    email     { 'ivan@gmail.com' }
-    role      { 'consumer' }
+  factory :user do
+    id          { 1 }
+    first_name  { 'James' }
+    password    { '123456' }
+    email       { 'ivan@gmail.com' }
+    role        { 'consumer' }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /sign_up' do
-    it 'returns http success' do
-      get '/users/sign_up'
-      expect(response).to have_http_status(:success)
+  context 'when valid path' do
+    describe 'GET /sign_up' do
+      it 'returns http success' do
+        get '/users/sign_up'
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe 'GET /sight_in' do
-    it 'returns http success' do
-      get '/users/sign_in'
-      expect(response).to have_http_status(:success)
+    describe 'GET /sight_in' do
+      it 'returns http success' do
+        get '/users/sign_in'
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
