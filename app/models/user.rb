504f4a9cce138b0d46c2cce200b8a86747961027
Kostyantyn <kostp62020@gmail.com>
2021-06-01 18:50:36 +0300

    add password_confirmation validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, :password_confirmation, presence: true, length: { minimum: 6 }
 end
