220fd0f52fce981c2848b7d8953eee6de9ed5cad
RMuskovets <rmuskovets@gmail.com>
2021-06-01 08:50:22 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,28 @@ ActiveRecord::Schema.define(version: 2021_05_25_071219) do
     t.string "place_type"
     t.text "description"
     t.float "price"
-    t.boolean "status"
+    t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "FirstName", default: "", null: false
+    t.string "LastName", default: "", null: false
+    t.string "role", default: "consumer", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
