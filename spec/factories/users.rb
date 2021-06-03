24769e6e085bbd3cbbc9995ace16b58531dad770
Kostyantyn <kostp62020@gmail.com>
2021-06-03 14:49:12 +0300

    add password_confirmation for factories users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     id          { 1 }
     first_name  { 'James' }
-    password    { '123456' }
+    password     { '123456' }
+    password_confirmation    { '123456' }
     email       { 'ivan@gmail.com' }
     role        { 'consumer' }
   end
