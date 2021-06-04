947563b18b226d9f7f2e743c9ef92e340c9214f8
Kostyantyn <kostp62020@gmail.com>
2021-06-04 19:13:12 +0300

    fixed naming variables in rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,43 +2,43 @@ require 'rails_helper'
 
 RSpec.describe User do
   context 'when valid' do
-    let(:valid_user) { build(:user, first_name: 'a' * 4) }
-    let(:valids_user) { build(:user, role: 'owner') }
+    let(:user_without_name) { build(:user, first_name: 'a' * 4) }
+    let(:user_without_name_role) { build(:user, role: 'owner') }
 
     it 'creates an default user' do
-      expect(valid_user).to be_valid
+      expect(user_without_name).to be_valid
     end
 
     it 'creates an user with owner role' do
-      expect(valids_user).to be_valid
+      expect(user_without_name_role).to be_valid
     end
   end
 
   context 'when not valid' do
-    let(:unvalid1_user) { build(:user, first_name: '    ') }
-    let(:unvalid2_user) { build(:user, first_name: '1' * 100) }
-    let(:unvalid3_user) { build(:user, email: '') }
-    let(:unvalid4_user) { build(:user, password: '') }
-    let(:unvalid5_user) { build(:user, role: '') }
+    let(:user_without_name_empty) { build(:user, first_name: '    ') }
+    let(:user_without_name_unlimitted) { build(:user, first_name: '1' * 100) }
+    let(:user_without_email) { build(:user, email: '') }
+    let(:user_without_password) { build(:user, password: '') }
+    let(:user_without_role) { build(:user, role: '') }
 
     it 'creates an user with space-name' do
-      expect(unvalid1_user).not_to be_valid
+      expect(user_without_name_empty).not_to be_valid
     end
 
     it 'creates an user with long name' do
-      expect(unvalid2_user).not_to be_valid
+      expect(user_without_name_unlimitted).not_to be_valid
     end
 
     it 'creates an user without email' do
-      expect(unvalid3_user).not_to be_valid
+      expect(user_without_email).not_to be_valid
     end
 
     it 'creates an user without password' do
-      expect(unvalid4_user).not_to be_valid
+      expect(user_without_password).not_to be_valid
     end
 
     it 'creates an user without role' do
-      expect(unvalid5_user).not_to be_valid
+      expect(user_without_role).not_to be_valid
     end
   end
 end
