fc1bb1680b4ff280ed8adf8ad7034010c4923931
Kostyantyn <kostp62020@gmail.com>
2021-06-04 19:11:18 +0300

    removed doesn't need User

--- spec/mailers/previews/user_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-  def new_user_email
-    # Set up a temporary user for the preview
-    user = User.new(FirstName: 'Ivan', LastName: 'Bogun', email: 'ivan@gmail.com', message: 'I want to log in!')
-
-    UserMailer.with(user: user).new_user_email
-  end
-end
