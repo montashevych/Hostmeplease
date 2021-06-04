ee6a72240081c37bb472e261a792fcdcd82517c2
Kostyantyn <kostp62020@gmail.com>
2021-06-04 21:12:41 +0300

    add ENV variables for heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,17 +69,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'https://hostmeplease.herokuapp.com/' #replace with your own url
-  config.action_mailer.default_url_options = { host: host }
-  heroku config:set GOOGLE_LOGIN="aleksanderis2121@gmail.com"
-  heroku config:set GOOGLE_PASSWORD="onbvxknmqrvczbpk"
+  
+  config.action_mailer.default_url_options = { host: ENV['HEROK_HOST'] }
     
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => GOOGLE_LOGIN,
-    :password             => GOOGLE_PASSWORD,
+    :user_name            => ENV['GMAIL_LOGIN'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
