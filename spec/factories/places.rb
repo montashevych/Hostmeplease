a7341467a268b86433a3ea6fc11d2f7b911f4609
Kostyantyn <kostp62020@gmail.com>
2021-06-04 20:39:59 +0300

    removed factories places because it rewrite in other branch

--- spec/factories/places.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :place do
-    title { Faker::Company.name }
-    type { 'office' }
-    description { Faker::Lorem.paragraphs }
-    price { Faker::Number.number(digits: 4) }
-    status { true }
-  end
-end
