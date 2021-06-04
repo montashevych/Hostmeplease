15e44cf4cf416b05c3c29964bc85029468c80956
Ivan Novak <76266630+VanyaNovak@users.noreply.github.com>
2021-06-04 21:25:41 +0300

    Update place_spec.rb

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,9 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  let(:test_place) { create(:place) }
-
-  it 'is valid with valid attributes' do
-    expect(test_place).to be_valid
-  end
 end
