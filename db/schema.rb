5fb4bd1ad500feb20da72f1d7b17fbd8c1ccd6d9
Ivan <vananovak@ukr.net>
2021-06-07 00:40:19 +0300

    change font weight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_062318) do
+ActiveRecord::Schema.define(version: 2021_05_25_071219) do
 
   create_table "places", force: :cascade do |t|
     t.string "title"
