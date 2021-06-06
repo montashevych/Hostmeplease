ae0724d6824938e99812e584b784944adcc9943f
Kostyantyn <kostp62020@gmail.com>
2021-06-06 11:00:40 +0300

    removed all place's files

--- app/controllers/places_controller.rb
+++
@@ -1,7 +0,0 @@
-class PlacesController < ApplicationController
-  PLACES_PER_PAGE = 9
-
-  def index
-    @places = Place.where(status: true).paginate(page: params[:page], per_page: PLACES_PER_PAGE)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  resources :places
   devise_for :users
   resources :users
 end
