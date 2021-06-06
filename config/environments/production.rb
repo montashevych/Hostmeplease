ca717c0beb10cbc9d016f3477b327dc89f13ddb8
Kostyantyn <kostp62020@gmail.com>
2021-06-06 23:14:36 +0300

    changed ENV variable for production Host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.default_url_options = { host: ENV['HEROK_HOST'] }
+  config.action_mailer.default_url_options = { host: ENV['HEROKU_HOST'] }
     
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
