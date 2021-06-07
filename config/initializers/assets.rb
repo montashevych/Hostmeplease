e3458ec30b73ae23fb180f165bed27f22ec4dd2a
Ivan <vananovak@ukr.net>
2021-06-07 09:16:45 +0300

    depoly

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
+Rails.application.config.assets.precompile += [/.*\.js/,/.*\.scss/]
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
