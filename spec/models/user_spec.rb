15f0466387efcb4ad7617d0d6fe6462f76870e5e
Kostyantyn <kostp62020@gmail.com>
2021-06-07 11:49:26 +0300

    add two tests for password's empty and password less than sixth symbols

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe User do
   context 'when valid' do
     let(:user_with_name_of_four_length) { build(:user, first_name: 'a' * 4) }
+    let(:user_with_password_of_sixth_length) { build(:user, password: 'a' * 6) }
     let(:user_without_name_role) { build(:user, role: 'owner') }
 
-    it 'creates an default user' do
+    it 'creates an default user with indication name' do
       expect(user_with_name_of_four_length).to be_valid
+    end
+
+    it 'creates an default user with indication password' do
+      expect(user_with_password_of_sixth_length).to be_valid
     end
 
     it 'creates an user with owner role' do
@@ -16,7 +21,7 @@ RSpec.describe User do
 
   context 'when not valid' do
     let(:user_with_empty_name) { build(:user, first_name: '    ') }
-    let(:user_with_long_name) { build(:user, first_name: '1' * 100) }
+    let(:user_with_long_name) { build(:user, first_name: '1' * 51) }
     let(:user_without_email) { build(:user, email: '') }
     let(:user_without_password) { build(:user, password: '') }
     let(:user_without_role) { build(:user, role: '') }
@@ -39,6 +44,14 @@ RSpec.describe User do
 
     it 'creates an user without role' do
       expect(user_without_role).not_to be_valid
+    end
+  end
+
+  context 'when not valid password' do
+    let(:user_with_long_password) { build(:user, password: '1' * 51) }
+
+    it 'creates an user with long password' do
+      expect(user_with_long_password).not_to be_valid
     end
   end
 end
