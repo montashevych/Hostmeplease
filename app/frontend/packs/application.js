ff8644792506bd55dd61cca8d5d116c14b9c6074
Ivan Novak <76266630+VanyaNovak@users.noreply.github.com>
2021-06-08 17:46:56 +0300

    Update application.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,7 +4,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import '../js/bootstrap_js_files.js'
 import "javascript/channels"
-//import "channels"
 
 Rails.start()
 Turbolinks.start()
