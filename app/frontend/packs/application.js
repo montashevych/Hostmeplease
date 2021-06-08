01778d6a112ef8592790e830d27df186cd196e1a
Ivan <vananovak@ukr.net>
2021-06-08 13:41:07 +0300

    Fixed stylesheets

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,1 +1,13 @@
-require.context('../../assets/images', true)
+require.context('../images', true)
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import '../js/bootstrap_js_files.js'
+import "javascript/channels"
+//import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import '../stylesheets/application.scss'
