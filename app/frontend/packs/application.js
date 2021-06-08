e15b6cd65e894dbfe6e5768d6a9691dea26f0625
Andriy Byalyk <andriy.byalyk@deliveroo.co.uk>
2021-06-08 21:38:58 +0300

    Remove popper definition from application.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,7 +2,6 @@ require.context('../images', true)
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import 'popper.js'
 import '../js/bootstrap_js_files.js'
 import "javascript/channels"
 
