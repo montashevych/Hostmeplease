9d5df0cc8d242b9e6abc02e064f23b213176e054
Ivan <vananovak@ukr.net>
2021-06-08 17:50:46 +0300

    Delete null false from migration

--- db/migrate/20210525071219_create_places.rb
+++ db/migrate/20210525071219_create_places.rb
@@ -2,7 +2,7 @@ class CreatePlaces < ActiveRecord::Migration[6.1]
   def change
     create_table :places do |t|
       t.string :title, null: false
-      t.string :type, null: false
+      t.string :type
       t.text :description, null: false
       t.float :price, default: 0, null: false
       t.integer :status, null: false
