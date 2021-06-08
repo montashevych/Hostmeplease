e48dc92c3d1c858ec6b20c2a7d1424a26ff7347f
Kostyantyn <kostp62020@gmail.com>
2021-06-08 14:32:02 +0300

    fixed some popper.js problem

--- app/frontend/packs/site.js
+++ app/frontend/packs/site.js
@@ -6,8 +6,10 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "popper.js"
 import '../js/bootstrap_js_files.js'
 import "javascript/channels"
+
 
 Rails.start()
 Turbolinks.start()
