63a93e0b22f170d150e88f2f1196ae0a10558f9f
Andriy Byalyk <andriy.byalyk@deliveroo.co.uk>
2021-06-08 21:26:55 +0300

    Fix popper js import

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,6 +2,7 @@ require.context('../images', true)
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'popper.js'
 import '../js/bootstrap_js_files.js'
 import "javascript/channels"
 
