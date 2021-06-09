336b46727b2e78a26f57be1bf893b8352b19f031
Ivan <vanuha277@gmail.com>
2021-06-09 15:44:10 +0300

    rubocop changes

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe PlacesController do
   describe PlacesController do
-
     it 'render template index and partials' do
       get :index
       expect(response).to render_template 'places/index'
