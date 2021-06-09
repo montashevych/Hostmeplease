4954924b6c467f5e71558ef76feb588f883d5235
Ivan <vanuha277@gmail.com>
2021-06-09 15:42:29 +0300

    some title changies

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PlacesController do
   describe PlacesController do
-    it 'test rendering template' do
+
+    it 'render template index and partials' do
       get :index
       expect(response).to render_template 'places/index'
     end
