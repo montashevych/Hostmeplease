6b099a013ee32d6094a7818da0cc952d876cbcc8
Ivan <vanuha277@gmail.com>
2021-06-10 20:31:54 +0300

    refactoring))

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,12 @@
 module PlacesHelper
+  def address(plc)
+    data = Geocoder.search([plc.lon, plc.lat]).first.data
+    str = data.extract!('address').values.first.extract!('country',
+                                                           'state',
+                                                           'city',
+                                                           'village',
+                                                           'road',
+                                                           'house_number')
+    str.values.join(', ')
+  end
 end
