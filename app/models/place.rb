e312c793bf56e2fb7a0c6288abdb3819d2a30ffb
Ivan <vanuha277@gmail.com>
2021-06-10 23:00:44 +0300

    added validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,8 @@ class Place < ApplicationRecord
   validates :title, presence: true, length: { maximum: 23 }
   validates :description, length: { maximum: 600 }
   validates :price, presence: true
+  validates :lon, presence: true
+  validates :lat, presence: true
 
   scope :workspaces, -> { where(type: 'Workspace') }
   scope :accommodations, -> { where(type: 'Accommodation') }
