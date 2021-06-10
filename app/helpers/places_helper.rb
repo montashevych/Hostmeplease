9177432c0bf3194ab698380935378fec1dde1863
Ivan <vanuha277@gmail.com>
2021-06-10 20:46:26 +0300

    rubocop requirements

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -2,11 +2,11 @@ module PlacesHelper
   def address(plc)
     data = Geocoder.search([plc.lon, plc.lat]).first.data
     str = data.extract!('address').values.first.extract!('country',
-                                                           'state',
-                                                           'city',
-                                                           'village',
-                                                           'road',
-                                                           'house_number')
+                                                         'state',
+                                                          'city',
+                                                          'village',
+                                                          'road',
+                                                          'house_number')
     str.values.join(', ')
   end
 end
