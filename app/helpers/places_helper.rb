3dec8df214dc48ee8486940c0e52aa568c29ec1c
Ivan <vanuha277@gmail.com>
2021-06-12 16:59:43 +0300

    removed method for address by geocoder

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,9 +1,2 @@
 module PlacesHelper
-  SLICED_ATTRIBUTES = %w[country state city village road house_number].freeze
-
-  def full_address(place)
-    data = Geocoder.search([place.lon, place.lat]).first.data
-    str = data.extract!('address').values.first.extract!(*SLICED_ATTRIBUTES)
-    str.values.join(', ')
-  end
 end
