fcbf7016a93274c669f8934be73dace6894e0686
Ivan <vanuha277@gmail.com>
2021-06-12 16:57:10 +0300

    changed migration

--- db/migrate/20210610142955_add_coordinates_columns.rb
+++ db/migrate/20210610142955_add_coordinates_columns.rb
@@ -2,5 +2,7 @@ class AddCoordinatesColumns < ActiveRecord::Migration[6.1]
   def change
     add_column :places, :lon, :float, default: 0.0
     add_column :places, :lat, :float, default: 0.0
+    add_column :places, :address, :string
+
   end
 end
