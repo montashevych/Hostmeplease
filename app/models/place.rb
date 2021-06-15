98bc35f69e42021338dcbf50dc21545777d41b83
Kostyantyn <kostp62020@gmail.com>
2021-06-15 12:05:54 +0300

    add relationship for user

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
+  # has_many :pictures, as: :imageable
   enum status: { created: 0, updated: 1, approved: 2 }
 
   validates :title, presence: true, length: { maximum: 23 }
@@ -8,4 +9,5 @@ class Place < ApplicationRecord
 
   scope :workspaces, -> { where(type: 'Workspace') }
   scope :accommodations, -> { where(type: 'Accommodation') }
+  # mount_uploader :picture_url, ImageUploader
 end
