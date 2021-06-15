5a2e8b49c6db3b45ce46b3a8f6c10a72175781e6
Kostyantyn <kostp62020@gmail.com>
2021-06-15 12:35:05 +0300

    resolved conflicts master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_142955) do
+ActiveRecord::Schema.define(version: 2021_06_13_194200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,10 +34,10 @@ ActiveRecord::Schema.define(version: 2021_06_10_142955) do
     t.boolean "is_active", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.float "lon", default: 0.0
     t.float "lat", default: 0.0
     t.string "address"
+    t.bigint "user_id"
     t.index ["type"], name: "index_places_on_type"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
