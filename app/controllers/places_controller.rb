f4589fd541c27bc966328bf075af87b7d2d4e5fc
Kostyantyn <kostp62020@gmail.com>
2021-06-15 12:06:40 +0300

    add action myplaces for show view My Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,25 @@
 class PlacesController < ApplicationController
   PLACES_PER_PAGE = 9
-
+  PLACES_PER_MY_PAGE = 8
+  before_action :place_find, only: [:show]
   def index
     @places = Place.where(status: :created).paginate(page: params[:page], per_page: PLACES_PER_PAGE)
   end
 
-  def show
-    place_find
-  end
+  def show;  end
 
   def myplaces
-    @place = Place.where(user: current_user)
+    @count_places = current_user.places.count
+    @places = current_user.places.paginate(page: params[:page], per_page: PLACES_PER_MY_PAGE)
   end
 
   private
 
   def place_find
     @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit( :title )
   end
 end
