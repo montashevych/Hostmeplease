913aac9eb3ecd13c3fbf7f43145de3fa353c9847
RMuskovets <rmuskovets@gmail.com>
2021-06-19 10:21:34 +0300

    Fix rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe User do
     let(:user_with_name_of_two_length) { build(:user, first_name: 'a' * 2) }
     let(:user_with_password_of_sixth_length) { build(:user, password: 'a' * 6) }
     let(:user_without_name_role) { build(:user, role: 'owner') }
-    let(:user_from_google) { User.from_google(email: 'test@mail.com', params: { password: 'a'*6, first_name:'a'*2 }) }
+    let(:user_from_google) {
+      described_class.from_google(email: 'test@mail.com', params: { password: 'a' * 6, first_name: 'a' * 2 })
+    }
 
     it 'creates an default user with indication name' do
       expect(user_with_name_of_two_length).to be_valid
@@ -53,19 +55,19 @@ RSpec.describe User do
 
     it 'creates an user without params' do
       expect {
-        User.from_google(email: 'test@mail.com', params: {})
+        described_class.from_google(email: 'test@mail.com', params: {})
       }.to raise_error(ArgumentError) # with empty params create_with(**params) takes 0, it is supposed to take >= 1
     end
 
     it 'creates a user without name' do
       expect {
-        User.from_google(email: 'test@mail.com', params: { password: 'a'*6 })
+        described_class.from_google(email: 'test@mail.com', params: { password: 'a' * 6 })
       }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'creates a user without password' do
       expect {
-        User.from_google(email: 'test@mail.com', params: { first_name: 'a'*2 })
+        described_class.from_google(email: 'test@mail.com', params: { first_name: 'a' * 2 })
       }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
