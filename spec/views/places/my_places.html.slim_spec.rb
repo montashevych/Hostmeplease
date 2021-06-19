8849e2ad5af4965e1b07f33c9a84007fe474a7fe
Kostyantyn <kostp62020@gmail.com>
2021-06-19 16:14:50 +0300

    add some empty line

--- spec/views/places/my_places.html.slim_spec.rb
+++ spec/views/places/my_places.html.slim_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'places/my_places.html.slim', type: :view do
   context 'when template rendered' do
+
     it 'infers the controller path' do
       expect(controller.controller_path).to eq('places')
     end
