5183c78af95d6c8ac9dcefe4477c079d5a2bd67d
Ivan <vanuha277@gmail.com>
2021-06-21 23:41:23 +0300

    responsible style added

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,12 +36,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
-
   def content_type_whitelist
-    /image\//
+    %r{image/}
   end
 
   # Override the filename of the uploaded files:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                ENV['AWS_REGION'],
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION'],
   }
-  config.fog_directory  = ENV['AWS_BUCKET_NAME']
+  config.fog_directory = ENV['AWS_BUCKET_NAME']
 end
