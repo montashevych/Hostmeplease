89670781f33ab98e5bb0d65cbc3a566da46225ff
Kostyantyn <kostp62020@gmail.com>
2021-06-24 14:06:01 +0300

    refactoring code

--- spec/views/places/_my_place.html.slim_spec.rb
+++ spec/views/places/_my_place.html.slim_spec.rb
@@ -1,20 +1,35 @@
 require 'rails_helper'
+require 'will_paginate/array'
 
-RSpec.describe 'shared/_my_place.html.slim', type: :view do
+RSpec.describe 'places/my_places.html.slim', type: :view do
   context 'when template rendered' do
     it 'infers the controller path' do
-      expect(controller.controller_path).to eq('shared')
+      expect(controller.controller_path).to eq('places')
     end
   end
-end
 
-RSpec.describe PlacesController, 'shared/my_place.html.slim', type: :controller do
-  context "with render_views(false) nested in a group with render_views" do
-    let!(:test_user) { FactoryBot.build(:user) }
-    let!(:test_place) { FactoryBot.create(:place, user: test_user) }
+  context 'with render_views(false) nested in a group with render_views' do
+    let(:test_user) { FactoryBot.build(:user) }
+    let(:test_place) { FactoryBot.create(:place, user: test_user) }
 
-    it "renders the RSpec generated template" do
-      expect(response.body).to eq("")
+    it 'renders the RSpec generated template' do
+      expect(response.body).to eq('')
+    end
+  end
+
+  context 'when was successfull status' do
+    let(:user) { FactoryBot.create(:user, :confirmed) }
+    let(:test_place) { FactoryBot.create(:place, user: user) }
+
+    before do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+    end
+
+    it 'render partial' do
+      @places = [test_place].paginate
+      sign_in user
+      render
+      expect(rendered).to match(/Workspace/)
     end
   end
 end
