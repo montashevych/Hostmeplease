dd03d5cb5df92edd67f6f8b0ac2ac47ea3290def
RMuskovets <rmuskovets@gmail.com>
2021-06-24 12:44:33 +0300

    My profile tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:test_user) { build :user }
+
   context 'when valid path' do
+
     describe 'GET /sign_up' do
       it 'returns http success' do
         get '/users/sign_up'
@@ -13,6 +16,23 @@ RSpec.describe 'Users', type: :request do
       it 'returns http success' do
         get '/users/sign_in'
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET /users/{id}' do
+      it 'redirects to sign in' do
+        get "/users/#{test_user.id}"
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to('/users/sign_in')
+      end
+    end
+  end
+
+  context 'when invalid path' do
+    describe 'GET /users/{id}' do
+      it 'doesnt return http success' do
+        get "/users/foobar"
+        expect(response).not_to have_http_status(:success)
       end
     end
   end
