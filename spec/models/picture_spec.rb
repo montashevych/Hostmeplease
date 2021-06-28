94013e704658375fe4b6e361e7bff612be2d7d07
Ivan <vanuha277@gmail.com>
2021-06-28 21:20:18 +0300

    corrected picture validation test

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
   let(:test_picture) { FactoryBot.build(:picture) }
+  let(:test_place) { FactoryBot.create(:place) }
+
+  before do
+    test_picture.imageable_id = test_place.id
+    test_picture.save!
+  end
 
   it 'is valid with valid attributes' do
     expect(test_picture).to be_valid
