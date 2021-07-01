1fde15f722a3559622fec1eb6b82773deec50393
RMuskovets <rmuskovets@gmail.com>
2021-07-01 12:20:51 +0300

    Add controller actions for bookings

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,64 @@
 class PlacesController < ApplicationController
+  respond_to :json, :html
+
   PLACES_PER_PAGE = 9
 
   def index
     @places = Place.where(status: :created).paginate(page: params[:page], per_page: PLACES_PER_PAGE)
   end
 
+  def bookings
+    @place = Place.find(params[:id])
+
+    respond_with do |format|
+      format.json { render json: @place.bookings.where(cancelled: false) }
+    end
+  end
+
   def show
     @place = Place.find(params[:id])
+
+    respond_with do |format|
+      format.html
+      format.json { render json: @place }
+    end
+  end
+
+  def book
+    @place = Place.find(params[:place_id])
+    @user  = current_user
+
+    unless booking_overlaps(params[:booking][:checkin], params[:booking][:checkout])
+      redirect_to_confirm
+    else
+      show_overlap_flash
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:checkin, :checkout)
+  end
+
+  def booking_overlaps(checkin, checkout)
+    overlapping = Booking
+      .with_overlapping_dates(checkin, checkout)
+      .where(place_id: @place.id, cancelled: false)
+    
+    overlapping.any?
+  end
+
+  def show_overlap_flash
+    flash[:error] = "Another user has already booked #{@place.title} for that dates."
+    redirect_to place_url(id: @place.id)
+  end
+
+  def redirect_to_confirm
+    @booking = Booking.create(place: @place, user: @user, confirmed: false)
+    @booking.update!(booking_params)
+    @booking.save!
+
+    redirect_to booking_confirm_url(id: @booking.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
   resources :places
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users
+
+  get  '/users/:id/bookings'   , to: 'users#bookings'  , as: :user_bookings
+  get  '/places/:id/bookings'  , to: 'places#bookings' 
+  get  '/bookings/:id'         , to: 'bookings#show'   , as: :booking
+  get  '/bookings/:id/confirm' , to: 'bookings#confirm', as: :booking_confirm
+  get  '/bookings/:id/receipt' , to: 'bookings#receipt', as: :booking_receipt
+  get  '/bookings/:id/save'    , to: 'bookings#save'   , as: :booking_save
+  post '/bookings/:id/cancel'  , to: 'bookings#cancel' , as: :booking_cancel
+  post '/places/:place_id/book', to: 'places#book'     , as: :place_book
 end
