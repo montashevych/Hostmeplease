cc24ce579848bed77190e179cf36fc6aaf6ca9c6
Ivan <vanuha277@gmail.com>
2021-07-03 16:17:17 +0300

    connected leaflet as module by yarn

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,6 +5,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import '../js/bootstrap_js_files.js'
 import '../js/newplace.js.erb'
 import "javascript/channels"
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 Rails.start()
 Turbolinks.start()
