015cecb6020e285cb1f334cf63dbf606077380cf
Ivan <vanuha277@gmail.com>
2021-07-04 13:57:56 +0300

    converted function to pack and connected this

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,7 +3,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import '../js/bootstrap_js_files.js'
-import '../js/newplace.js.erb'
+// import '../js/newplace.js.erb'
 import "javascript/channels"
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
