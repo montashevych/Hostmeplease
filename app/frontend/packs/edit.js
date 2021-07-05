6f04bfcb7a6403b7660869cd034c7db845814b4f
Kostyantyn <kostp62020@gmail.com>
2021-07-05 21:14:40 +0300

    add phone_number js code for correct works

--- app/frontend/packs/edit.js
+++ app/frontend/packs/edit.js
@@ -1,24 +1,20 @@
 import intlTelInput from 'intl-tel-input';
 
-window.onload = () => {
+document.addEventListener("turbolinks:load", function(event) {
   const input = document.querySelector("#phone");
-  console.log(input);
   intlTelInput(input, {
       autoHideDialCode: true,
       autoPlaceholder: "off",
       dropdownContainer: document.body,
       formatOnDisplay: false,
-      // geoIpLookup: function(success, failure) {
-      //   $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-      //   var countryCode = (resp && resp.country) ? resp.country : "us";
-      //   success(countryCode);
-      // });
-      // },
+      formatOnInit: true,
       // hiddenInput: "full_number",
       initialCountry: "auto",
       localizedCountries: { 'de': 'Deutschland' },
       nationalMode: false,
       placeholderNumberType: "MOBILE",
-
-  });
-}
+      geoIpLookup: (ok,fail) => { fetch("https://ipinfo.io/198.16.66.101?token=a109fee2e4e92d")
+        .then(response => response.json())
+        .then(({ country }) => ok(country || "us")) }
+      });
+});
