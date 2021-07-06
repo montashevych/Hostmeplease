28792a6bc607b6640baa7bf5e0e4f831394b1a9b
Kostyantyn <kostp62020@gmail.com>
2021-07-06 23:04:58 +0300

    migrations were carried out

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_07_06_094507) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar_url"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
