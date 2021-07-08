171d91cb1f5824d70c0d13a5477cf5e9c2617e80
Ivan <vanuha277@gmail.com>
2021-07-08 12:42:56 +0300

    removed array type

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :picture do
     image {
-      [Rack::Test::UploadedFile.new(
+      Rack::Test::UploadedFile.new(
                           File.open(Rails.root.join('spec/factories/test.png')),
                           'image/png',
-                        )]
+                        )
     }
     imageable_type { 'Place' }
   end
