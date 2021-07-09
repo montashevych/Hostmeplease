a55a3945e989d3c3d543ab26c48217a2d4091ce4
Kostyantyn <kostp62020@gmail.com>
2021-07-09 20:11:38 +0300

    add user last_name and phone_number validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,14 @@ class User < ApplicationRecord
          omniauth_providers: [:google_oauth2]
   enum role: { consumer: 'consumer', owner: 'owner' }
 
-  validates :first_name, length: { minimum: 2, maximum: 50 }
+  validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :role, presence: true
   validates :email, presence: true,
                     format: { with: email_regexp },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6, maximum: 256, on: :create }
   validates :password, confirmation: { case_sensitive: true }
+  validates :phone_number, presence: true, length: { minimum: 8, maximum: 17 }
 
   def self.from_google(email:, params:)
     create_with(**params).find_or_create_by!(email: email)
