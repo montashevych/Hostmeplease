5bf68a34d3ff512a840828cb0370239f55e3b52b
Ivan <vanuha277@gmail.com>
2021-07-10 20:43:11 +0300

    added tests

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  context 'when  valid' do
-    let(:test_user) { FactoryBot.build(:user) }
-    let(:test_place) { FactoryBot.create(:place, user: test_user) }
+  let(:test_user) { FactoryBot.build(:user) }
+  let(:test_place) { FactoryBot.create(:place, user: test_user) }
 
+  context 'when  valid' do
     it 'is valid with valid attributes' do
       expect(test_place).to be_valid
     end
   end
 
   context 'when length incorrect' do
-    let(:test_user) { FactoryBot.build(:user) }
-    let(:test_place) { FactoryBot.create(:place, user: test_user) }
-
     it 'does not allow a title longer than 50 characters' do
       test_place.title = 'q' * 24
       expect(test_place).not_to be_valid
@@ -31,6 +28,18 @@ RSpec.describe Place, type: :model do
 
     it 'does not allow a description shorter than 20 characters' do
       test_place.description = 'q' * 19
+      expect(test_place).not_to be_valid
+    end
+  end
+
+  context 'without relation' do
+    it 'to user not valid' do
+      test_place.user = nil
+      expect(test_place).not_to be_valid
+    end
+
+    it 'to address not valid' do
+      test_place.address = nil
       expect(test_place).not_to be_valid
     end
   end
