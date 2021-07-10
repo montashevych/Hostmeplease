0633fc0964aba40c37898394a0bf9f8bbde5f2aa
Ivan <vanuha277@gmail.com>
2021-07-10 20:42:58 +0300

    refactoring and added tests

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
   let!(:test_user) { FactoryBot.create(:user) }
-  let!(:test_picture) { FactoryBot.build(:picture) }
   let!(:test_place) { FactoryBot.create(:place, user: test_user) }
+  let!(:test_picture) { FactoryBot.build(:picture, imageable: test_place) }
 
   before do
-    test_picture.imageable_id = test_place.id
     test_picture.save!
   end
 
@@ -18,12 +17,26 @@ RSpec.describe Picture, type: :model do
     expect(test_picture).to be_valid
   end
 
-  it 'change role to owner' do
-    expect(test_user.role).to eq('owner')
+  context 'when the user role changes to' do
+    it 'owner' do
+      expect(test_user.role).to eq('owner')
+    end
+
+    it 'consumer' do
+      test_user.places.first.destroy
+      expect(test_user.role).to eq('consumer')
+    end
   end
 
-  it 'change user\'s role to consumer' do
-    test_user.places.first.destroy
-    expect(test_user.role).to eq('consumer')
+  context 'without presence not valid' do
+    it 'to image' do
+      test_picture.image = nil
+      expect(test_picture).not_to be_valid
+    end
+
+    it 'to place reference' do
+      test_picture.imageable = nil
+      expect(test_picture).not_to be_valid
+    end
   end
 end
