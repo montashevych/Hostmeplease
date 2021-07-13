39d878eea292d0fbba33c710347474fa2f0a60d6
Ivan <vanuha277@gmail.com>
2021-07-13 22:24:29 +0300

    removed trash

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,7 +3,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import '../js/bootstrap_js_files.js'
-// import '../js/newplace.js.erb'
 import "javascript/channels"
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
