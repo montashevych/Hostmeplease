c089161c9567c87bfc0ccf6d7b11c4ee644b6337
Kostyantyn <kostp62020@gmail.com>
2021-07-16 16:06:12 +0300

    fixed for correct user_form tests

--- spec/models/user_form_spec.rb
+++ spec/models/user_form_spec.rb
@@ -2,52 +2,47 @@ require 'rails_helper'
 
 RSpec.describe UserForm do
   context 'when not valid' do
-    let(:form) { UserForm.new(user: user, **parameters) }
-    let(:parameters_first_name) { { first_name: 'a' } }
-    let(:parameters_long_first_name) { { first_name: 'a' * 51 } }
-    let(:parameters_last_name) { { last_name: 'a' } }
-    let(:parameters_long_last_name) { { last_name: 'a' * 51 } }
+    let!(:user) { FactoryBot.create(:user) }
+    let(:form) { -> (parameters) { UserForm.new(user: user, **parameters) } }
 
     it 'creates an default user with indication first_name' do
-      form.parameters_first_name
-      expect(form).not_to be_valid
+      parameters = { first_name: 'a' }
+      expect(form.call(parameters)).not_to be_valid
+    end
+
+    it 'creates an default user with long fist_name' do
+      parameters = { first_name: 'a' * 51 }
+      expect(form.call(parameters)).not_to be_valid
     end
 
     it 'creates an default user with indication last_name' do
-      form.parameters_last_name
-      expect(form).not_to be_valid
-    end
-
-    it 'creates an default user with long last_name' do
-      form.parameters_long_first_name
-      expect(form).not_to be_valid
+        parameters = { last_name: 'a' }
+      expect(form.call(parameters)).not_to be_valid
     end
 
     it 'creates an user with long last_name' do
-      form.parameters_long_last_name
-      expect(form).not_to be_valid
+      parameters = { last_name: 'a' * 51 }
+      expect(form.call(parameters)).not_to be_valid
     end
   end
 
-  context 'when not valid phone_number' do
-    let(:form) { UserForm.new(user: user, **parameters) }
-    let(:parameters_email) { { email: ' ' } }
-    let(:parameters_phone_number) { { phone_number: '1' * 7 } }
-    let(:parameters_long_phone_number) { { phone_number: '1' * 18 } }
+  context 'when not valid parameters' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:form) { -> (parameters) { UserForm.new(user: user, **parameters) } }
 
     it 'creates an user email' do
-      form.parameters_email
-      expect(form).not_to be_valid
+      parameters = { email: ' ' }
+      expect(form.call(parameters)).not_to be_valid
     end
 
     it 'creates an  user with short phone_number' do
-      form.parameters_phone_number
-      expect(form).not_to be_valid
+      parameters = { phone_number: 'a' * 7 }
+      expect(form.call(parameters)).not_to be_valid
     end
 
     it 'creates an user with long phone_number' do
-      form.parameters_long_phone_number
-      expect(form).not_to be_valid
+      parameters = { phone_number: 'a' * 18 }
+      expect(form.call(parameters)).not_to be_valid
     end
   end
 end
