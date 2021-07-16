3b2ff8dbf46da38f4de7d67ca6e4b3f16fea6edc
Ivan <vanuha277@gmail.com>
2021-07-16 20:06:50 +0300

    code style

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  validates :image, presence: true, file_size: { less_than: 1.megabytes }
-
   belongs_to :imageable, polymorphic: true, inverse_of: :pictures
+
+  validates :image, presence: true, file_size: { less_than: 1.megabytes }
 end
