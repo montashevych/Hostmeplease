c15fd859659392f2f3c9139ba7b3930887a59c95
Ivan <vanuha277@gmail.com>
2021-07-17 21:37:15 +0300

    allowed svg files

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,10 +33,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [600, 600]
   end
 
+  version :avatar do
+    process resize_to_fit: [135, 135]
+  end
+
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w[jpg jpeg gif png]
+    %w[jpg jpeg gif png svg]
   end
 
   def content_type_allowlist
