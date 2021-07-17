e73b4ad73c8d038f5b3f83733db629f38e362626
Ivan <vanuha277@gmail.com>
2021-07-17 21:44:38 +0300

    removed trash

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,6 @@
 require 'digest'
 
 module UsersHelper
-  def gravatar_for(user, size: 40)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.first_name, class: 'gravatar')
-  end
-
   def avatar_for(user, size = 40)
     if user.avatar_url && user.avatar_url =~ (GOOGLE_AVATAR_STORAGE_REGEX)
       image_tag(google_avatar_url_for(user, size), alt: user.first_name,
