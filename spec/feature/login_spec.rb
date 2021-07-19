1d47002aa29b55833ca6717c173aae7b9d128983
Kostyantyn <kostp62020@gmail.com>
2021-07-19 14:59:48 +0300

    refactoring and write correct tests to sign_in and sign_up form

--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -1,29 +1,27 @@
 require 'rails_helper'
 
 describe 'the sign_in process', type: :feature do
-  include ApplicationHelper
   context 'when user can' do
-    let(:test_user) { FactoryBot.create(:user, :confirmed) }
-
-    before do
-      test_registration
-      test_confirmation
-      test_login
-    end
+    let(:test_user) { create(:user, :confirmed) }
+    let(:user) { build(:user) }
 
     it 'create new session' do
+      visit new_user_session_path
+      fill_in 'Email', with: test_user.email
+      fill_in 'Password', with: user.password
+      click_button 'Sign In'
       have_current_path eq(root_path)
       expect(page).to have_text('Signed in successfully.')
     end
 
-    # it 'sign_up' do
-    #   visit '/users/sign_up'
-    #   fill_in 'user[first_name]', with: test_user.first_name
-    #   fill_in 'user[email]', with: test_user.email
-    #   fill_in 'user[password]', with: test_user.password
-    #   fill_in 'user[password_confirmation]', with: test_user.password_confirmation
-    #   click_button 'Sign Up'
-    #   expect(current_path).to eq(root_path)
-    # end
+    it 'sign_up' do
+      visit new_user_registration_path
+      fill_in 'user[first_name]', with: user.first_name
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password_confirmation
+      click_button 'Sign Up'
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
   end
 end
