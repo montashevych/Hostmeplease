706f9299fc3cc355ca341850aea634f6bf6de8ea
RMuskovets <rmuskovets@gmail.com>
2021-07-19 12:02:21 +0300

    Fix again

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :place
   belongs_to :user
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+
   validate :cannot_overlap_with_other_booking
 
   scope :with_overlapping_dates, lambda { |checkin, checkout|
